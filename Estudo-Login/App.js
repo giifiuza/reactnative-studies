1542eaf815115229b17eacb4c91506c5474ccd7f
Giovanna Fiuza <94703688+giifiuza@users.noreply.github.com>
2023-08-21 16:15:56 -0300

    Update App.js

--- Estudo-Login/App.js
+++ Estudo-Login/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
-import App from './ThirdPage';
 import { View } from 'react-native-animatable';
 
 
 export default function App() {
   return (
-    <View>
-      <App />
-    </View>
-    // <NavigationContainer>
-    //   <StatusBar backgroundColor="#38a69d" barStyle="light-content"/>
-    //   <Routes/>
-    // </NavigationContainer>
+    
+    <NavigationContainer>
+      <StatusBar backgroundColor="#38a69d" barStyle="light-content"/>
+      <Routes/>
+    </NavigationContainer>
   );
 }
